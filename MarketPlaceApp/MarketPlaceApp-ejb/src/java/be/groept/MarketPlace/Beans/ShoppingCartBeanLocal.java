4b1541344292738e4e3521a0fb91d2ea19b547bb
Ghysa <samghysels@hotmail.com>
2015-04-24 16:26:24 +0200

    Shopping Cart + Item

---
+++ MarketPlaceApp/MarketPlaceApp-ejb/src/java/be/groept/MarketPlace/Beans/ShoppingCartBeanLocal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.groept.MarketPlace.Beans;
+
+import be.groept.MarketPlace.Entities.Item;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Sam
+ */
+@Local
+public interface ShoppingCartBeanLocal {
+
+    void addItem(Item item);
+    
+}
